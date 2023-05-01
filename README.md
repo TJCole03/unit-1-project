b3b77660afd68115f095820cf8765670b6ffbe80
TJCole03 <tjc6254@gmail.com>
2023-04-30 20:29:43 -0400

    Managed to shuffle cards

--- README.md
+++ README.md
@@ -1,1 +1,81 @@
+// class Cards {
+//     constructor(for(let i = 1; i < 13; i++), color) {
+//     this.color = color
+
+//     }
+// }
+
+// class Spade extends Cards {
+//     constructor(..args) {
+//         this.color = black; 
+//     }
+// }
+
+// class Club extends Cards {
+//     constructor(..args) {
+//         this.color = black; 
+//     }
+// }
+// class Heart extends Cards {
+//     constructor(..args) {
+//         this.color = red; 
+//     }
+// }
+// class Diamond extends Cards {
+//     constructor(..args) {
+//         this.color = red; 
+//     }
+// }
+
+//making the cards https://www.youtube.com/watch?v=LxXWTXOny3A, https://www.thatsoftwaredude.com/content/6196/coding-a-card-deck-in-javascript
+
+// const suits = ['Hr','Cb','Dr','Sb']; //r = red, b = black
+// const rank = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+// //let deck = [];
+
+// function getDeck() {
+//     let deck = new Array();
+//     for (let i = 0; i < suits.length; i++) {
+//         for (let x = 0; x < rank.length; x++) {
+//             //console.log(rank[rankCounter] + suits[suitCounter]);
+//             let card = { Rank: rank[x], Suit: suits[i] };
+//             deck.push(card);
+//         }
+//     }
+//     return deck;
+// }
+
+
+// function shuffle(deck) {
+
+//     //for 1000 turns
+//     //switch values of two random cards
+//     for (let i = 0; i < 1000; i++){
+//         let spot1 = Math.floor((Math.random() * deck.length));
+//         let spot2 = Math.floor((Math.random() * deck.length));
+//         let tmp = deck[spot1]
+
+//         deck[spot1] = deck[spot2];
+//         deck[spot2] = tmp;
+//     }
+// }
+
+// function renderDeck(deck) {
+//     document.getElementById("deck").innerHTML = "";
+
+//     for (let i = 0; i < deck.length; i++) {
+//         let card = document.createElement("div");
+//         let value = document.createElement("div");
+//         let suit = document.createElement("div");
+//         card.className = "card";
+//         value.className = "value";
+//         suit.className = "suit " + deck[i].Suit;
+
+//         value.innerHTML = deck[i].Value;
+//         card.appendChild(value);
+//         card.appendChild(suit);
+
+//         document.getElementById("deck").appendChild(card);
+//     }
+// }
 
--- app.js
+++ app.js
@@ -1,50 +1,67 @@
+//https://www.youtube.com/watch?v=h3ehO5QMNu4  https://www.youtube.com/watch?v=NxRwIZWjLtE //will use this one 
 
-// class Cards {
-//     constructor(for(let i = 1; i < 13; i++), color) {
-//     this.color = color
-
-//     }
-// }
-
-// class Spade extends Cards {
-//     constructor(..args) {
-//         this.color = black; 
-//     }
-// }
-
-// class Club extends Cards {
-//     constructor(..args) {
-//         this.color = black; 
-//     }
-// }
-// class Heart extends Cards {
-//     constructor(..args) {
-//         this.color = red; 
-//     }
-// }
-// class Diamond extends Cards {
-//     constructor(..args) {
-//         this.color = red; 
-//     }
-// }
-
-//making the cards https://www.youtube.com/watch?v=LxXWTXOny3A, https://www.thatsoftwaredude.com/content/6196/coding-a-card-deck-in-javascript
-
-const suits = ['Hr','Cb','Dr','Sb']; //r = red, b = black
-const rank = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-
-
-function getDeck() {
-    let deck = new Array();
-    for (let i = 0; i < suits.length; i++) {
-        for (let x = 0; x < rank.length; x++) {
-            //console.log(rank[rankCounter] + suits[suitCounter]);
-            let card = { Rank: rank[x], Suit: suits[i] };
-            deck.push(card);
-        }
+class Card {
+    constructor(suit, value) {
+        this.suit = suit; 
+        this.value = value; 
     }
-    return deck;
-    
 }
-let deck = [{Rank: 'A', Suit: 'Spades'}, {Rank: 'A', Suit: 'Diamonds'}, {Rank: 'A', Suit: 'Clubs'}...]
 
+class Deck {
+    constructor() {
+        this.deck = [];
+    }
+
+
+    createDeck(suits, values) {
+        for (let suit of suits) {
+            for (let value of values) {
+                this.deck.push(new Card(suit, value));
+            }
+        }
+        return this.deck;
+    }
+
+    shuffle() {
+        let counter = this.deck.length, temp, i;
+
+        while (counter) {
+            i = Math.floor(Math.random() * counter--); 
+            temp = this.deck[counter];
+            this.deck[counter] = this.deck[i];
+            this.deck[i] = temp; 
+        }
+        return this.deck;
+    }
+
+    deal() { 
+        
+    }
+
+}
+
+let suits = ['H', 'D', 'C', 'S'];
+let values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+let deck = new Deck();
+deck.createDeck(suits, values)
+deck.shuffle()
+
+ function renderDeck(deck) {
+    document.getElementById("deck").innerHTML = "";
+
+    for (let i = 0; i < deck.length; i++) {
+        let card = document.createElement("div");
+        let value = document.createElement("div");
+        let suit = document.createElement("div");
+        card.className = "card";
+        value.className = "value";
+        suit.className = "suit " + deck[i].Suit;
+
+        value.innerHTML = deck[i].Value;
+        card.appendChild(value);
+        card.appendChild(suit);
+
+        document.getElementById("deck").appendChild(card);
+    }
+     return this.deck;
+}
