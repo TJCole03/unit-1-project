f405d970d0b77b597e12323ed5fc8fba774c7673
TJCole03 <tjc6254@gmail.com>
2023-05-02 07:45:48 -0400

    Last edits of the night

--- app.js
+++ app.js
@@ -5,19 +5,48 @@ console.log(deck.cards)
 
 //CLASSES
 
-//VARIABLES 
-let stockDeck = []
+//OBJECTS AND ARRAYS
+
+let stock =  [];
+let waste = [''];
+let foundations = {
+    spade: [],
+    diamond: [],
+    club: [],
+    heart: [],
+}
+let tableau = {
+     arr0: [],
+     arr1: [],
+     arr2: [],
+     arr3: [],
+     arr4: [],
+     arr5: [],
+     arr6: [],
+}
+
 const startGame = document.querySelector('.start-button');
+const stockDeck = document.getElementById('#stock');
+const wasteDeck = document.getElementById('#waste');
+const foundationTable = document.getElementById('#foundations');
+const foundSpade = document.getElementsByClassName('.spade');
+const foundDiamond = document.getElementsByClassName('.diamond');
+const foundClub = document.getElementsByClassName('.club');
+const foundHeart = document.getElementsByClassName('.heart');
 
-//EVENT LISTENER 
 
-startGame.addEventListener('click', function(dealCards) {
-    console.log('clicked');
+//EVENT LISTENERS
+
+startGame.addEventListener('click', function () {
+  
+    //dealCards();
+    console.log('clicked')
 });
 
 function dealCards() {
-    stockDeck = document.querySelector("stock")
-    stockDeck = document.createElement()
+    const dealCards = stock.concat(deck);
+    
+    return dealCards; 
 }
 
 
